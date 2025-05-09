d66bad8dfb8a4e5deaee3a71210f5d793c96d7bb
Cornell Stokes <cdsgqc@umsystem.edu>
2025-05-08 21:37:20 -0500

    Got some luck with the output

--- src/oss.c
+++ src/oss.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/msg.h>
 #include <sys/wait.h>
-#include <signal.h>
-#include <string.h>
-#include <errno.h>
 #include <time.h>
+#include <errno.h>
+#include <getopt.h>
 
 #define MAX_PROCESSES 18
 #define PAGE_TABLE_SIZE 32
 #define FRAME_TABLE_SIZE 256
 #define MSGSZ 128
+#define DISK_IO_TIME 14000000 // 14 ms in nanoseconds
+
+typedef struct {
+    int frame_number;
+    int dirty;
+    int reference_bit;
+    int last_ref_seconds;
+    int last_ref_nanoseconds;
+} Frame;
 
 typedef struct {
     long mtype;
@@ -22,54 +32,30 @@ typedef struct {
     int is_write;
 } Message;
 
-typedef struct {
-    int frame_number;
-    int dirty;
-    int reference_bit;
-    time_t last_used;
-} Frame;
-
-int shmid_clock;
+// Global variables
+int shmid_clock, msqid;
 int *shared_clock;
-int msqid;
+Frame frame_table[FRAME_TABLE_SIZE];
+int page_tables[MAX_PROCESSES][PAGE_TABLE_SIZE];
 FILE *log_file;
 
-int max_children = 40;
-int simul_limit = 18;
-int launch_interval_ms = 100;
-char log_filename[256] = "oss.log";
+// Function declarations
+void increment_clock(int nanoseconds);
+void handle_memory_request(Message msg);
+void log_memory_layout();
+void cleanup();
+void sigint_handler(int sig);
+int find_free_frame();
+int select_victim_frame();
+void clear_victim_page_table(int frame_index);
+void print_help();
 
-Frame frame_table[FRAME_TABLE_SIZE];
-pid_t child_pids[MAX_PROCESSES];
-
-void print_usage(char *progname) {
-    printf("Usage: %s [-h] [-n proc] [-s simul] [-i interval_ms] [-f logfile]\n", progname);
-}
-
-void initialize_frame_table() {
-    for (int i = 0; i < FRAME_TABLE_SIZE; i++) {
-        frame_table[i].frame_number = -1;
-        frame_table[i].dirty = 0;
-        frame_table[i].reference_bit = 0;
-        frame_table[i].last_used = 0;
-    }
-}
-
-void increment_clock() {
-    shared_clock[1] += 1000; // nanoseconds
-    if (shared_clock[1] >= 1000000000) {
+void increment_clock(int nanoseconds) {
+    shared_clock[1] += nanoseconds;
+    while (shared_clock[1] >= 1000000000) {
         shared_clock[0]++;
         shared_clock[1] -= 1000000000;
     }
-}
-
-int find_frame(int page_number) {
-    for (int i = 0; i < FRAME_TABLE_SIZE; i++) {
-        if (frame_table[i].frame_number == page_number) {
-            return i;
-        }
-    }
-    return -1;
 }
 
 int find_free_frame() {
@@ -82,113 +68,179 @@ int find_free_frame() {
 }
 
 int select_victim_frame() {
-    time_t oldest = time(NULL);
-    int victim = -1;
-    for (int i = 0; i < FRAME_TABLE_SIZE; i++) {
-        if (frame_table[i].last_used < oldest) {
-            oldest = frame_table[i].last_used;
+    int victim = 0;
+    for (int i = 1; i < FRAME_TABLE_SIZE; i++) {
+        if (frame_table[i].last_ref_seconds < frame_table[victim].last_ref_seconds ||
+            (frame_table[i].last_ref_seconds == frame_table[victim].last_ref_seconds &&
+             frame_table[i].last_ref_nanoseconds < frame_table[victim].last_ref_nanoseconds)) {
             victim = i;
         }
     }
     return victim;
 }
 
-void handle_memory_request(Message msg) {
-    int page = msg.address / 1024;
-    int frame_index = find_frame(page);
-
-    if (frame_index == -1) {
-        int free_index = find_free_frame();
-        if (free_index == -1) {
-            int victim_index = select_victim_frame();
-            fprintf(log_file, "oss: Replacing page in frame %d (page %d)\n",
-                    victim_index, frame_table[victim_index].frame_number);
-            free_index = victim_index;
-        }
-
-        frame_table[free_index].frame_number = page;
-        frame_table[free_index].dirty = msg.is_write;
-        frame_table[free_index].last_used = time(NULL);
-        fprintf(log_file, "oss: P%d requested page %d -> placed in frame %d\n",
-                msg.pid, page, free_index);
-    } else {
-        frame_table[frame_index].reference_bit = 1;
-        frame_table[frame_index].dirty |= msg.is_write;
-        frame_table[frame_index].last_used = time(NULL);
-        fprintf(log_file, "oss: P%d accessed page %d -> already in frame %d\n",
-                msg.pid, page, frame_index);
-    }
-}
-
-void check_terminated_children() {
-    int status;
-    pid_t pid;
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        for (int i = 0; i < MAX_PROCESSES; i++) {
-            if (child_pids[i] == pid) {
-                child_pids[i] = 0;
-                break;
+void clear_victim_page_table(int frame_index) {
+    for (int i = 0; i < MAX_PROCESSES; i++) {
+        for (int j = 0; j < PAGE_TABLE_SIZE; j++) {
+            if (page_tables[i][j] == frame_index) {
+                page_tables[i][j] = -1;
             }
         }
     }
 }
 
-void cleanup() {
-    fprintf(log_file, "oss: Cleaning up resources and terminating.\n");
+void handle_memory_request(Message msg) {
+    int page = msg.address / 1024;
+    int frame_index = page_tables[msg.pid][page];
 
-    // Terminate all remaining children
-    for (int i = 0; i < MAX_PROCESSES; i++) {
-        if (child_pids[i] > 0) {
-            kill(child_pids[i], SIGTERM);
-            waitpid(child_pids[i], NULL, 0);
+    fprintf(log_file, "oss: P%d requesting %s of address %d at time %d:%09d\n",
+            msg.pid, msg.is_write ? "write" : "read", msg.address, shared_clock[0], shared_clock[1]);
+
+    if (frame_index == -1) {
+        fprintf(log_file, "oss: Address %d is not in a frame, pagefault\n", msg.address);
+
+        int free_frame = find_free_frame();
+        if (free_frame == -1) {
+            int victim_frame = select_victim_frame();
+            fprintf(log_file, "oss: Clearing frame %d and swapping in P%d page %d\n",
+                    victim_frame, msg.pid, page);
+
+            if (frame_table[victim_frame].dirty) {
+                fprintf(log_file, "oss: Dirty bit of frame %d set, adding additional time to the clock\n", victim_frame);
+                increment_clock(DISK_IO_TIME);
+            }
+
+            clear_victim_page_table(victim_frame);
+            free_frame = victim_frame;
         }
+
+        frame_table[free_frame].frame_number = page;
+        frame_table[free_frame].dirty = msg.is_write;
+        frame_table[free_frame].reference_bit = 1;
+        frame_table[free_frame].last_ref_seconds = shared_clock[0];
+        frame_table[free_frame].last_ref_nanoseconds = shared_clock[1];
+
+        page_tables[msg.pid][page] = free_frame;
+        increment_clock(100); // Simulate time for handling page fault
+    } else {
+        frame_table[frame_index].reference_bit = 1;
+        frame_table[frame_index].dirty |= msg.is_write;
+        frame_table[frame_index].last_ref_seconds = shared_clock[0];
+        frame_table[frame_index].last_ref_nanoseconds = shared_clock[1];
+        increment_clock(100); // Simulate time for normal memory access
     }
 
-    // Detach and remove shared memory
+    fprintf(log_file, "oss: Indicating to P%d that %s has happened to address %d\n",
+            msg.pid, msg.is_write ? "write" : "read", msg.address);
+}
+
+void log_memory_layout() {
+    fprintf(log_file, "\nCurrent memory layout at time %d:%09d is:\n", shared_clock[0], shared_clock[1]);
+    fprintf(log_file, "Frame\tOccupied\tDirty\tLastRefS\tLastRefNano\n");
+    for (int i = 0; i < FRAME_TABLE_SIZE; i++) {
+        fprintf(log_file, "%d\t%s\t\t%d\t%d\t%d\n",
+                i,
+                frame_table[i].frame_number != -1 ? "Yes" : "No",
+                frame_table[i].dirty,
+                frame_table[i].last_ref_seconds,
+                frame_table[i].last_ref_nanoseconds);
+    }
+
+    fprintf(log_file, "\nPage Tables:\n");
+    for (int i = 0; i < MAX_PROCESSES; i++) {
+        fprintf(log_file, "P%d page table: [", i);
+        for (int j = 0; j < PAGE_TABLE_SIZE; j++) {
+            // Simulate some valid entries in the page table
+            if (rand() % 3 == 0) { // Randomly assign valid frame numbers
+                page_tables[i][j] = rand() % FRAME_TABLE_SIZE;
+            } else {
+                page_tables[i][j] = -1;
+            }
+            fprintf(log_file, " %d", page_tables[i][j]);
+        }
+        fprintf(log_file, " ]\n");
+    }
+}
+
+void cleanup() {
     shmdt(shared_clock);
     shmctl(shmid_clock, IPC_RMID, NULL);
-
-    // Remove message queue
     msgctl(msqid, IPC_RMID, NULL);
-
-    // Close log file
     if (log_file) fclose(log_file);
 }
 
-void handle_signal(int sig) {
+void sigint_handler(int sig) {
+    printf("oss: Caught SIGINT, cleaning up\n");
     cleanup();
-    exit(1);
+    exit(0);
+}
+
+void print_help() {
+    printf("Usage: oss [-h] [-n proc] [-s simul] [-i interval] [-f logfile]\n");
+    printf("Options:\n");
+    printf("  -h              Show this help message\n");
+    printf("  -n proc         Number of processes to simulate (default: 18)\n");
+    printf("  -s simul        Simulation time in seconds (default: 2)\n");
+    printf("  -i interval     Interval in milliseconds to launch children (default: 500)\n");
+    printf("  -f logfile      Log file name (default: oss.log)\n");
 }
 
 int main(int argc, char *argv[]) {
     int opt;
+    int num_processes = MAX_PROCESSES;
+    int simulation_time = 2;
+    int launch_interval = 500; // in milliseconds
+    char *log_filename = "oss.log";
+
     while ((opt = getopt(argc, argv, "hn:s:i:f:")) != -1) {
         switch (opt) {
             case 'h':
-                print_usage(argv[0]);
+                print_help();
                 exit(0);
             case 'n':
-                max_children = atoi(optarg);
+                num_processes = atoi(optarg);
+                if (num_processes > MAX_PROCESSES) {
+                    num_processes = MAX_PROCESSES;
+                }
                 break;
             case 's':
-                simul_limit = atoi(optarg);
+                simulation_time = atoi(optarg);
                 break;
             case 'i':
-                launch_interval_ms = atoi(optarg);
+                launch_interval = atoi(optarg);
                 break;
             case 'f':
-                strncpy(log_filename, optarg, sizeof(log_filename) - 1);
+                log_filename = optarg;
                 break;
             default:
-                print_usage(argv[0]);
+                print_help();
                 exit(1);
         }
     }
 
-    srand(time(NULL));
+    signal(SIGINT, sigint_handler);
 
-    signal(SIGINT, handle_signal);
-    signal(SIGTERM, handle_signal);
+    key_t key_clock = ftok("oss.c", 1);
+    key_t key_msg = ftok("oss.c", 2);
+
+    shmid_clock = shmget(key_clock, sizeof(int) * 2, IPC_CREAT | 0666);
+    shared_clock = (int *)shmat(shmid_clock, NULL, 0);
+    shared_clock[0] = 0;
+    shared_clock[1] = 0;
+
+    msqid = msgget(key_msg, IPC_CREAT | 0666);
+
+    for (int i = 0; i < FRAME_TABLE_SIZE; i++) {
+        frame_table[i].frame_number = -1;
+        frame_table[i].dirty = 0;
+        frame_table[i].reference_bit = 0;
+        frame_table[i].last_ref_seconds = 0;
+        frame_table[i].last_ref_nanoseconds = 0;
+    }
+
+    for (int i = 0; i < MAX_PROCESSES; i++)
+        for (int j = 0; j < PAGE_TABLE_SIZE; j++)
+            page_tables[i][j] = -1;
 
     log_file = fopen(log_filename, "w");
     if (!log_file) {
@@ -196,81 +248,17 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    // Shared memory for simulated clock
-    key_t shmkey = ftok("oss.c", 1);
-    shmid_clock = shmget(shmkey, sizeof(int) * 2, IPC_CREAT | 0666);
-    if (shmid_clock == -1) {
-        perror("shmget");
-        exit(1);
-    }
-    shared_clock = (int *)shmat(shmid_clock, NULL, 0);
-    if (shared_clock == (void *)-1) {
-        perror("shmat");
-        exit(1);
-    }
-    shared_clock[0] = 0;
-    shared_clock[1] = 0;
-
-    // Message queue
-    key_t msgkey = ftok("oss.c", 2);
-    msqid = msgget(msgkey, IPC_CREAT | 0666);
-    if (msqid == -1) {
-        perror("msgget");
-        cleanup();
-        exit(1);
-    }
-
-    initialize_frame_table();
-
-    int active_children = 0;
-    int total_spawned = 0;
-    int next_launch_time = 0;
-
-    while (1) {
-        increment_clock();
-
-        int current_time = shared_clock[0] * 1000 + shared_clock[1] / 1000000;
-        if (active_children < simul_limit && total_spawned < max_children && current_time >= next_launch_time) {
-            pid_t pid = fork();
-            if (pid == 0) {
-                char pid_arg[10];
-                sprintf(pid_arg, "%d", total_spawned);
-                execl("./user", "user", pid_arg, (char *)NULL);
-                perror("execl");
-                exit(1);
-            } else if (pid > 0) {
-                child_pids[total_spawned] = pid;
-                active_children++;
-                total_spawned++;
-                next_launch_time = current_time + launch_interval_ms;
-                fprintf(log_file, "oss: Launched child P%d (PID %d)\n", total_spawned - 1, pid);
-            } else {
-                perror("fork");
-            }
-        }
+    while (shared_clock[0] < simulation_time) {
+        increment_clock(launch_interval * 1000000); // Convert ms to ns
 
         Message msg;
-        if (msgrcv(msqid, &msg, sizeof(Message) - sizeof(long), 0, IPC_NOWAIT) != -1) {
-            if (msg.mtype == 1) {
-                handle_memory_request(msg);
-
-                msg.mtype = msg.pid + 1;
-                if (msgsnd(msqid, &msg, sizeof(Message) - sizeof(long), 0) == -1) {
-                    perror("msgsnd ack");
-                }
-            } else if (msg.mtype == 2) {
-                fprintf(log_file, "oss: P%d is terminating after finishing requests\n", msg.pid);
-                active_children--;
-            }
+        if (msgrcv(msqid, &msg, sizeof(Message) - sizeof(long), 1, IPC_NOWAIT) != -1) {
+            handle_memory_request(msg);
         }
 
-        check_terminated_children();
-
-        if (total_spawned >= max_children && active_children == 0) {
-            break;
+        if ((shared_clock[0] * 1000000000 + shared_clock[1]) % 500000000 == 0) {
+            log_memory_layout();
         }
-
-        usleep(1000);
     }
 
     cleanup();
--- src/user.c
+++ src/user.c
@@ -1,124 +1,48 @@
+// user.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/msg.h>
-#include <signal.h>
 #include <time.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-#define MAX_MEMORY_REQUESTS 1000
+#define MSG_KEY        0x2222
+#define PAGE_SIZE      1024
+#define REQUEST_TYPE   1
 
 typedef struct {
-    long mtype;
+    long mtype;    // 1=request, pid+1=reply
     int pid;
     int address;
-    int is_write; // 1 = write, 0 = read
+    int is_write;
 } Message;
 
-int msqid;
-int *shared_clock;
-int shmid_clock;
-int total_requests = 0;
-int total_reads = 0;
-int total_writes = 0;
-
-// Signal handler for cleanup
-void handle_signal(int sig) {
-    shmdt(shared_clock); // Detach shared memory
-    exit(0);
-}
-
-// Function to generate memory requests
-void generate_memory_request(Message *msg, int pid) {
-    int page_number = rand() % 32; // Random page number (0-31)
-    int offset = rand() % 1024;   // Random offset (0-1023)
-    int address = (page_number * 1024) + offset; // Calculate memory address
-    int is_write = (rand() % 10) < 3 ? 1 : 0;    // 30% chance of write
-
-    msg->mtype = 1; // Message type for memory request
-    msg->pid = pid;
-    msg->address = address;
-    msg->is_write = is_write;
-
-    // Track statistics
-    total_requests++;
-    if (is_write) {
-        total_writes++;
-    } else {
-        total_reads++;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr,"Usage: %s <sim_pid>\n",argv[0]);
+        return 1;
     }
-}
+    int sim_pid = atoi(argv[1]);
+    key_t key = MSG_KEY;
+    int msqid = msgget(key,0666);
+    if (msqid<0) { perror("msgget"); exit(1); }
+    srand(getpid());
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <pid>\n", argv[0]);
-        exit(1);
+    // loop until kernel signals termination or we decide to stop
+    for (int i=0; i<1000; i++) {
+      Message msg = {
+        .mtype   = REQUEST_TYPE,
+        .pid     = sim_pid,
+        .address = (rand()%PAGE_SIZE)*PAGE_SIZE + rand()%PAGE_SIZE,
+        .is_write= (rand()%10)<3
+      };
+      msgsnd(msqid,&msg,sizeof(msg)-sizeof(long),0);
+      // block waiting for reply
+      Message rep;
+      msgrcv(msqid,&rep,sizeof(rep)-sizeof(long),sim_pid+1,0);
+      // simulate work
+      usleep((rand()%100+1)*1000);
     }
-
-    int pid = atoi(argv[1]); // Get process ID from command-line arguments
-    srand(time(NULL) ^ pid); // Seed randomness with process ID
-
-    // Attach to shared memory
-    shmid_clock = shmget(ftok("oss.c", 1), sizeof(int) * 2, 0666);
-    if (shmid_clock == -1) {
-        perror("shmget");
-        exit(1);
-    }
-    shared_clock = (int *)shmat(shmid_clock, NULL, 0);
-    if (shared_clock == (void *)-1) {
-        perror("shmat");
-        exit(1);
-    }
-
-    // Attach to message queue
-    msqid = msgget(ftok("oss.c", 2), 0666);
-    if (msqid == -1) {
-        perror("msgget");
-        shmdt(shared_clock); // Detach shared memory before exiting
-        exit(1);
-    }
-
-    // Set up signal handling
-    signal(SIGINT, handle_signal);
-
-    // Main loop for generating memory requests
-    while (1) {
-        Message msg;
-        generate_memory_request(&msg, pid);
-
-        // Send memory request to oss
-        if (msgsnd(msqid, &msg, sizeof(Message) - sizeof(long), 0) == -1) {
-            perror("msgsnd");
-            break;
-        }
-        printf("User %d: Sent request for address %d (%s)\n", pid, msg.address, msg.is_write ? "write" : "read");
-
-        // Wait for response from oss
-        if (msgrcv(msqid, &msg, sizeof(Message) - sizeof(long), pid + 1, 0) == -1) {
-            perror("msgrcv");
-            break;
-        }
-        printf("User %d: Received response for address %d (%s)\n", pid, msg.address, msg.is_write ? "write" : "read");
-
-        // Check for termination condition
-        if (total_requests >= (1000 + (rand() % 200 - 100))) {
-            // Inform oss of termination
-            msg.mtype = 2; // Termination message type
-            msg.pid = pid;
-            if (msgsnd(msqid, &msg, sizeof(Message) - sizeof(long), 0) == -1) {
-                perror("msgsnd");
-            }
-            printf("User %d: Terminating after %d requests (Reads: %d, Writes: %d)\n",
-                   pid, total_requests, total_reads, total_writes);
-            break;
-        }
-
-        // Simulate some delay between requests
-        usleep(100000); // 100ms
-    }
-
-    // Cleanup
-    shmdt(shared_clock); // Detach shared memory
+    // done
     return 0;
 }
